2b2757e6aed05a92c3ec7b9aad7a0b7f792544ca
casa-henrique <riquecasa@gmail.com>
2022-02-25 19:27:39 -0300

    Implementação de arquivos CSS

--- webpack.config.js
+++ webpack.config.js
@@ -1,8 +1,11 @@
 const path = require("path");
 const HtmlWebPackPlugin = require("html-webpack-plugin");
 
+const isDevelopment = process.env.NODE_ENV !== "production";
+
 module.exports = {
-  mode: "development",
+  mode: isDevelopment ? "development" : "production",
+  devtool: isDevelopment ? "eval-source-map" : "source-map",
   entry: path.resolve(__dirname, "src", "index.jsx"),
   output: {
     path: path.resolve(__dirname, "dist"),
@@ -11,7 +14,25 @@ module.exports = {
   resolve: {
     extensions: [".js", ".jsx"],
   },
+  devServer: {
+    port: 3000,
+    static: {
+      directory: path.resolve(__dirname, "app"),
+    },
+  },
+  plugins: [
+    new HtmlWebPackPlugin({
+      template: path.resolve(__dirname, "public", "index.html"),
+    }),
+  ],
   module: {
-    rules: [{ test: /\.jsx$/, exclude: /node_modules/, use: "babel-loader" }],
+    rules: [
+      { test: /\.jsx$/, exclude: /node_modules/, use: "babel-loader" },
+      {
+        test: /\.css$/,
+        exclude: /node_modules/,
+        use: ["style-loader", "css-loader"],
+      },
+    ],
   },
 };
